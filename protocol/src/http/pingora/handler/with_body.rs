9360b4b311610159dc6b11ab620d24c196412399
Shabana Baig <43451943+s-akhtar-baig@users.noreply.github.com>
2026-08-05 16:29:59 -0400

    refactor: consolidate reserved header stripping (#892)
    * refactor: consolidate reserved header stripping
    
    Signed-off-by: Shabana Baig <43451943+s-akhtar-baig@users.noreply.github.com>
    
    * Handle review comments
    
    Signed-off-by: Shabana Baig <43451943+s-akhtar-baig@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Shabana Baig <43451943+s-akhtar-baig@users.noreply.github.com>

--- protocol/src/http/pingora/handler/with_body.rs
+++ protocol/src/http/pingora/handler/with_body.rs
@@ -27,9 +27,9 @@ use tokio::sync::Semaphore;
 use tracing::debug;
 
 use super::{
-    adjust_compression, emit_request_metrics, fail_to_proxy, handle_connect_failure, logging_cleanup,
-    record_passive_health, request_body_filter, request_filter, response_body_filter, response_filter, upstream_peer,
-    upstream_request, via,
+    adjust_compression, emit_request_metrics, fail_to_proxy, handle_connect_failure, hop_by_hop::RemoveHeader as _,
+    logging_cleanup, record_passive_health, request_body_filter, request_filter, response_body_filter, response_filter,
+    upstream_peer, upstream_request, via,
 };
 use crate::http::pingora::context::PingoraRequestCtx;
 
@@ -217,7 +217,7 @@ impl ProxyHttp for PingoraHttpHandler {
     {
         let is_upgrade = session.is_upgrade_req();
         upstream_request::strip_hop_by_hop(upstream_request, is_upgrade);
-        upstream_request::strip_reserved_internal(upstream_request);
+        upstream_request.strip_reserved_internal();
         upstream_request::apply_rewritten_path(upstream_request, ctx)?;
         upstream_request::apply_mutated_content_length(upstream_request, ctx);
         let client_ver = ctx.client_http_version.unwrap_or(http::Version::HTTP_11);
